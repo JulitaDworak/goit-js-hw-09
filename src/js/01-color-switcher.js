e83db20fa4a1041695429585b96c7abc87e1a3d5
JulitaDworak <julita.dworak@gmail.com>
2023-04-11 15:35:42 +0200

    hw-09

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startBtn = document.querySelector('[data-start]')
+const stopBtn = document.querySelector('[data-stop]')
+const body  = document.querySelector('body')
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  
+
+  const bcgColor = ()=> {
+    timerId = setInterval(() => {
+     body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+startBtn.disabled = true
+  stopBtn.disabled = false
+  }
+
+  const bcgStopColor =()=> {
+    clearInterval(timerId);
+      console.log(`Interval with id ${timerId} has stopped!`);
+      startBtn.disabled = false
+      stopBtn.disabled = true
+    }
+    
+    
+    startBtn.addEventListener("click",bcgColor )
+    stopBtn.addEventListener("click",bcgStopColor )
+    console.log(getRandomHexColor())
 
--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,16 +1,95 @@
-console.log(`hello`);
-
-
 import flatpickr from 'flatpickr';
-
 import 'flatpickr/dist/flatpickr.min.css';
 
+
+const inputData = document.getElementById('datetime-picker')
+const daysToGo = document.querySelector('[data-days]')
+const hoursToGo = document.querySelector('[data-hours]')
+const minutesToGo = document.querySelector('[data-minutes]')
+const secondsToGo = document.querySelector('[data-seconds]')
+const startBtn = document.querySelector('button')
+
+let calendarData = 0;
+let countDown = null;
+let milisecondsLeft = 0;
+let timeToGo = {};
+
+const convertMs = ms => {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+};
+
+// add 0 before 
+const addLeadingZero = (value) => {
+if (value<10) {
+  return value.toString().padStart(2,'0');
+return value}
+}
+
+
+
+
+
+const endCount = () => {
+  clearInterval(countDown)
+  daysToGo.textContent = "00"
+  hoursToGo.textContent = "00"
+  minutesToGo.textContent = "00"
+  secondsToGo.textContent = "00"
+}
+
+
+
+const updateTimeToGo = () => {
+timeToGo = convertMs(milisecondsLeft);
+daysToGo.textContent = addLeadingZero(timeToGo.days)
+hoursToGo.textContent = addLeadingZero(timeToGo.hours)
+minutesToGo.textContent = addLeadingZero(timeToGo.minutes) 
+secondsToGo.textContent = addLeadingZero(timeToGo.seconds)
+milisecondsLeft -= 1000;
+if (milisecondsLeft <0) {
+  endCount();
+  return "koniec odliczania"
+}
+}
+
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    onClose(selectedDates) {
-      console.log(selectedDates[0]);
-    },
-  };
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    calendarData = selectedDates[0].getTime();
+    milisecondsLeft = calendarData - new Date().getTime();
+    if (milisecondsLeft<0) {
+      return window.alert('Please choose a date in the future')
+    }
+    else {updateTimeToGo()}
+
+    startBtn.disable = false
+  },
+};
+
+flatpickr(inputData, options);
+
+
+startBtn.addEventListener('click', () => {
+  milisecondsLeft = calendarData - new Date().getTime();
+  clearInterval(countDown);
+  countDown = setInterval(() => updateTimeToGo(), 1000); 
+  startBtn.disabled = true
+})

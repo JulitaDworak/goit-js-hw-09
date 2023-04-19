62bf5b0bb36959139e9d23afc8c15aefd23eb5d8
JulitaDworak <julita.dworak@gmail.com>
2023-04-19 06:51:49 +0200

    hw9

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,6 +7,7 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
   }
   
+  let timerId;
 
   const bcgColor = ()=> {
     timerId = setInterval(() => {
